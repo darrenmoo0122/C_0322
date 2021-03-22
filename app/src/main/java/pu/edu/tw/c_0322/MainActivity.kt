aba48be4dfd581e26fce9c3de41bdfea42219278
student <o1083002@gm.pu.edu.tw>
2021-03-22 15:01:46 +0800

    img and 樂透

--- app/src/main/java/pu/edu/tw/c_0322/MainActivity.kt
+++ app/src/main/java/pu/edu/tw/c_0322/MainActivity.kt
@@ -6,21 +6,37 @@ import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        img.setOnClickListener(this)
+        btn4.setOnClickListener(this)
     }
 
     fun Add(v: View) {
         if (v == btn1) {
             counter++
-        } else {
+        } else if (v == btn2) {
             counter += 2
+        } else {
+            counter = 0
         }
         txv.text = counter.toString()
     }
-}z
+
+    override fun onClick(v: View?) {
+        if (v == txv) {
+            counter++
+        } else if (v == img) {
+            counter += 2
+        } else {
+            counter = (1..100).random()
+            txv.text = counter.toString()
+        }
+        txv.text = counter.toString()
+    }
+}
